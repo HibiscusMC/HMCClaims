f5dacc70dd555d387aeb19a611fa1bf001413172
Ricardo <git@krzu.me>
2026-07-23 11:04:51 -0500

    fix: strictly check for left and right click

--- src/main/java/com/hibiscusmc/hmcclaims/gui/impl/ClaimListGui.java
+++ src/main/java/com/hibiscusmc/hmcclaims/gui/impl/ClaimListGui.java
@@ -175,13 +175,11 @@ public class ClaimListGui implements BaseGui {
 
             pagedGui.addIngredient((char) entry.getKey().intValue(), Item.builder()
                     .setItemProvider(icon.item())
-                    .addClickHandler(click -> {
-                        for (Action iconAction : click.clickType() == ClickType.LEFT ?
-                                icon.leftClickActions() :
-                                icon.rightClickActions()) {
-                            iconAction.execute(player);
-                        }
-                    })
+                    .addClickHandler(click -> (switch (click.clickType()) {
+                        case LEFT -> icon.leftClickActions();
+                        case RIGHT -> icon.rightClickActions();
+                        default -> List.<Action>of();
+                    }).forEach(action -> action.execute(player)))
                     .build());
         }
 
