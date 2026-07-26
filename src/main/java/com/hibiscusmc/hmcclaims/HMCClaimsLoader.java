dc0b7c7dfe153a361ae8fb21b0158297d95da75c
Ricardo <git@krzu.me>
2026-07-26 15:44:22 -0500

    fix: add maven central mirror repo to pluginloader

--- src/main/java/com/hibiscusmc/hmcclaims/HMCClaimsLoader.java
+++ src/main/java/com/hibiscusmc/hmcclaims/HMCClaimsLoader.java
@@ -5,6 +5,7 @@ import io.papermc.paper.plugin.loader.PluginLoader;
 import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.graph.Dependency;
+import org.eclipse.aether.repository.RemoteRepository;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings({"UnstableApiUsage"})
@@ -13,6 +14,7 @@ public final class HMCClaimsLoader implements PluginLoader {
     @Override
     public void classloader(@NotNull PluginClasspathBuilder builder) {
         MavenLibraryResolver resolver = new MavenLibraryResolver();
+        resolver.addRepository(new RemoteRepository.Builder("central", "default", MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR).build());
         resolver.addDependency(new Dependency(new DefaultArtifact("com.zaxxer:HikariCP:7.0.2"), null));
         resolver.addDependency(new Dependency(new DefaultArtifact("org.spongepowered:configurate-yaml:4.2.0"), null));
         resolver.addDependency(new Dependency(new DefaultArtifact("org.mariadb.jdbc:mariadb-java-client:3.5.7"), null));
